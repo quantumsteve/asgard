5220c7e4902952c70e7957cdbcf2db0d79105e59
Steven Hahn <hahnse@ornl.gov>
2021-05-24 10:01:59 -0400

    Work on adding tests
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- src/cblacs_grid_tests.cpp
+++ src/cblacs_grid_tests.cpp
@@ -1,4 +1,26 @@
 #include "cblacs_grid.hpp"
+#include "distribution.hpp"
 #include "tests_general.hpp"
 
-TEST_CASE("", "[cblacs_grid]") { cblacs_grid grid; }
+struct distribution_test_init
+{
+  distribution_test_init() { initialize_distribution(); }
+  ~distribution_test_init() { finalize_distribution(); }
+};
+
+#ifdef ASGARD_USE_MPI
+static distribution_test_init const distrib_test_info;
+#endif
+
+TEST_CASE("", "[cblacs_grid]")
+{
+  int myrank = get_rank();
+  int num_ranks = get_num_ranks();
+  cblacs_grid grid;
+  int myrow = grid.get_myrow();
+  int mycol = grid.get_mycol();
+  int local_rows = grid.local_rows(3, 256);
+  int local_cols = grid.local_cols(3, 256);
+  //std::cout << context << ' ' << myrow << ' ' << mycol << ' ' << local_rows << ' ' << local_cols << '\n';
+  //CHECK(false);
+}
